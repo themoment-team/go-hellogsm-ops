a7cd0e1ab7ae4c163d7242b044ff9feea893fa94
신희성 <vvwv3092@gmail.com>
2024-09-08 01:39:27 +0900

    fix: NULL 값을 string 타입으로 출력하지 않도록 변경

--- generate-dml/generate-middle-school-achievement.go
+++ generate-dml/generate-middle-school-achievement.go
@@ -22,9 +22,9 @@ func GenerateMiddleSchoolAchievementInsertQuery(rows int, graduateStatuses []_ty
 
 	buffer.WriteString("-- tb_middle_school_achievement" + "\n\n")
 
-	artsPhysicalSubjects := `["체육","미술","음악"]`
-	generalSubjects := `["국어","도덕","사회","역사","수학","과학","기술가정","영어"]`
-	newSubjects := `["프로그래밍"]`
+	artsPhysicalSubjects := `'["체육","미술","음악"]'`
+	generalSubjects := `'["국어","도덕","사회","역사","수학","과학","기술가정","영어"]'`
+	newSubjects := `'["프로그래밍"]'`
 
 	for i := 1; i <= rows; i++ {
 		graduateStatus := graduateStatuses[i-1]
@@ -35,32 +35,32 @@ func GenerateMiddleSchoolAchievementInsertQuery(rows int, graduateStatuses []_ty
 		switch graduateStatus {
 		case _type.CANDIDATE:
 			gedTotalScore = "NULL"
-			absentDays = fmt.Sprintf("%v", randomIntArray(3, 0, 3))
-			achievement_2_1 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			achievement_2_2 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			achievement_3_1 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
+			absentDays = fmt.Sprintf("%s%v%s", "'", randomIntArray(3, 0, 3), "'")
+			achievement_2_1 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			achievement_2_2 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			achievement_3_1 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
 			achievement_3_2 = "NULL"
-			artsPhysicalAchievement = fmt.Sprintf("%v", randomIntArray(9, 3, 5))
-			attendanceDays = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			volunteerTime = fmt.Sprintf("%v", randomIntArray(3, 0, 5))
+			artsPhysicalAchievement = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 3, 5), "'")
+			attendanceDays = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			volunteerTime = fmt.Sprintf("%s%v%s", "'", randomIntArray(3, 0, 5), "'")
 			freeSemester = "NULL"
-			liberalSystem = "자유학년제"
+			liberalSystem = "'자유학년제'"
 
 		case _type.GRADUATE:
 			gedTotalScore = "NULL"
-			absentDays = fmt.Sprintf("%v", randomIntArray(3, 0, 3))
-			achievement_2_1 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			achievement_2_2 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			achievement_3_1 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			achievement_3_2 = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			artsPhysicalAchievement = fmt.Sprintf("%v", randomIntArray(9, 3, 5))
-			attendanceDays = fmt.Sprintf("%v", randomIntArray(9, 1, 5))
-			volunteerTime = fmt.Sprintf("%v", randomIntArray(3, 0, 5))
+			absentDays = fmt.Sprintf("%s%v%s", "'", randomIntArray(3, 0, 3), "'")
+			achievement_2_1 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			achievement_2_2 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			achievement_3_1 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			achievement_3_2 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			artsPhysicalAchievement = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 3, 5), "'")
+			attendanceDays = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
+			volunteerTime = fmt.Sprintf("%s%v%s", "'", randomIntArray(3, 0, 5), "'")
 			freeSemester = "NULL"
 			liberalSystem = "자유학년제"
 
 		case _type.GED:
-			gedTotalScore = fmt.Sprintf("%d", rand.Intn(201)+400) // 400 ~ 600 사이의 랜덤 값
+			gedTotalScore = fmt.Sprintf("%s%d%s", "'", rand.Intn(201)+400, "'") // 400 ~ 600 사이의 랜덤 값
 			absentDays = "NULL"
 			achievement_2_1 = "NULL"
 			achievement_2_2 = "NULL"
@@ -78,7 +78,7 @@ func GenerateMiddleSchoolAchievementInsertQuery(rows int, graduateStatuses []_ty
 
 		query := fmt.Sprintf(
 			"INSERT INTO tb_middle_school_achievement (oneseo_id, ged_total_score, absent_days, achievement_1_2, achievement_2_1, achievement_2_2, achievement_3_1, achievement_3_2, arts_physical_achievement, arts_physical_subjects, attendance_days, free_semester, general_subjects, liberal_system, new_subjects, volunteer_time) "+
-				"VALUES (%d, '%s', '%s', 'NULL', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
+				"VALUES (%d, %s, %s, NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
 			i, gedTotalScore, absentDays, achievement_2_1, achievement_2_2, achievement_3_1, achievement_3_2, artsPhysicalAchievement, artsPhysicalSubjects,
 			attendanceDays, freeSemester, generalSubjects, liberalSystem, newSubjects, volunteerTime,
 		)
