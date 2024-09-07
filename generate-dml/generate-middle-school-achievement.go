f3e0625da401dd48c988191190649664e1a379e2
신희성 <vvwv3092@gmail.com>
2024-09-08 01:52:13 +0900

    fix: 과목 이름들 반복문 안으로 이동

--- generate-dml/generate-middle-school-achievement.go
+++ generate-dml/generate-middle-school-achievement.go
@@ -7,7 +7,6 @@ import (
 	_type "themoment-team/hellogsm-notice-server/generate-dml/type"
 )
 
-// 랜덤 배열 생성 함수
 func randomIntArray(length int, min int, max int) []int {
 	arr := make([]int, length)
 	for i := 0; i < length; i++ {
@@ -16,17 +15,17 @@ func randomIntArray(length int, min int, max int) []int {
 	return arr
 }
 
-// GraduateStatus에 따라 중학교 성취도 데이터를 생성하는 함수
 func GenerateMiddleSchoolAchievementInsertQuery(rows int, graduateStatuses []_type.GraduateStatus) string {
 	var buffer bytes.Buffer
 
 	buffer.WriteString("-- tb_middle_school_achievement" + "\n\n")
 
-	artsPhysicalSubjects := `'["체육","미술","음악"]'`
-	generalSubjects := `'["국어","도덕","사회","역사","수학","과학","기술가정","영어"]'`
-	newSubjects := `'["프로그래밍"]'`
-
 	for i := 1; i <= rows; i++ {
+
+		artsPhysicalSubjects := `'["체육","미술","음악"]'`
+		generalSubjects := `'["국어","도덕","사회","역사","수학","과학","기술가정","영어"]'`
+		newSubjects := `'["프로그래밍"]'`
+
 		graduateStatus := graduateStatuses[i-1]
 		var gedTotalScore string
 		var absentDays, achievement_2_1, achievement_2_2, achievement_3_1, achievement_3_2, artsPhysicalAchievement, attendanceDays, volunteerTime string
@@ -53,14 +52,14 @@ func GenerateMiddleSchoolAchievementInsertQuery(rows int, graduateStatuses []_ty
 			achievement_2_2 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
 			achievement_3_1 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
 			achievement_3_2 = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
-			artsPhysicalAchievement = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 3, 5), "'")
+			artsPhysicalAchievement = fmt.Sprintf("%s%v%s", "'", randomIntArray(12, 3, 5), "'")
 			attendanceDays = fmt.Sprintf("%s%v%s", "'", randomIntArray(9, 1, 5), "'")
 			volunteerTime = fmt.Sprintf("%s%v%s", "'", randomIntArray(3, 0, 5), "'")
 			freeSemester = "NULL"
-			liberalSystem = "자유학년제"
+			liberalSystem = "'자유학년제'"
 
 		case _type.GED:
-			gedTotalScore = fmt.Sprintf("%s%d%s", "'", rand.Intn(201)+400, "'") // 400 ~ 600 사이의 랜덤 값
+			gedTotalScore = fmt.Sprintf("%s%d%s", "'", rand.Intn(201)+400, "'")
 			absentDays = "NULL"
 			achievement_2_1 = "NULL"
 			achievement_2_2 = "NULL"
