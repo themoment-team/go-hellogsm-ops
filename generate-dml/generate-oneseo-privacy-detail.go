bf7fa358ccf27cfdb7a5f54dcd945dcf3359194e
신희성 <vvwv3092@gmail.com>
2024-09-08 01:30:18 +0900

    fix: graduationDate 앞자리 7자리만 추출

--- generate-dml/generate-oneseo-privacy-detail.go
+++ generate-dml/generate-oneseo-privacy-detail.go
@@ -38,7 +38,7 @@ func GenerateOneseoPrivacyDetailInsertQuery(rows int, graduateStatuses []_type.G
 			"INSERT INTO tb_oneseo_privacy_detail (oneseo_id, address, detail_address, graduation_type, guardian_name, guardian_phone_number, profile_img, relationship_with_guardian, school_address, school_name, school_teacher_name, school_teacher_phone_number, graduation_date) "+
 				"VALUES (%d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
 			i, address, detailAddress, graduateStatus, guardianName, guardianPhoneNumber, profileImg, relationshipWithGuardian,
-			schoolAddress, schoolName, schoolTeacherName, schoolTeacherPhoneNumber, graduationDate,
+			schoolAddress, schoolName, schoolTeacherName, schoolTeacherPhoneNumber, graduationDate[:7],
 		)
 
 		buffer.WriteString(query + "\n")
