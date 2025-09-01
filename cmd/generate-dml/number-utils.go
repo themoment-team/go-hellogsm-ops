8f90ace61e8e87868f361b340d88f80a35d5a201
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-09-01 16:14:12 +0900

    chore: 사용하지 않는 메서드 삭제

--- cmd/generate-dml/number-utils.go
+++ cmd/generate-dml/number-utils.go
@@ -9,10 +9,6 @@ func generateFloatPointer(min, max float64) *float64 {
 	floatValue := min + (max-min)*rand.Float64()
 	return &floatValue
 }
-func generateIntPointer(min, max int) *int {
-	intValue := rand.Intn(max-min+1) + min
-	return &intValue
-}
 func formatNullableFloat(value *float64, scale int) string {
 	if value == nil {
 		return "NULL"
